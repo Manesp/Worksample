ec3a021e35729fe542d5d694edfca069c6728d6c
Manesp <mespina98@gmail.com>
2020-06-07 20:50:42 -0300

    Script con comentarios detallados y readme.md con el instructivo completo

--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
 # Instrusctivo del bucket
- - Requisitos:
-    - Instalar comando 
+ - ###  Requisitos:
+    - Instalar comando ~ apt install python3
     - Instalar AWS: Ejecutar comando #pip install aws
     - Tener key y secret key de AWS
 
-1) Descargar script y ejecutar
+1) Descargar script y ejecutar comando -python3 samplech.py desde el terminal
 
 2) Introducir key y secret key proveidas por AWS
 
@@ -12,4 +12,13 @@
 
 4) Seleccionar acción a realizar, subir, descargar o salir de la aplicación.
 
+    - ### Subida de archivos
+        1) Introducir elección "1"
+        2) Introducir el nombre de del archivo que desea subir
+    - ### Descarga de archivos
+        1) Introducir elección "2"
+        2) Introducir el nombre de del archivo que desea descargar
+
 5) Ejecutar acción.
+
+Los archivos descargados quedarán por defecto descargados en la carpeta desde la que ejecutamos el script bajo el nombre "default".
--- samplech.py
+++ samplech.py
@@ -6,10 +6,11 @@ from botocore.client import Config
 
 # Ingreso de credenciales.
 print("Ingrese credenciales.")
-access_key_id = "AKIAVVLED4RIT567M4TQ" #input("Acces key id: ")
-secret_access_key = "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" #input("Secret Key: ")
-nbucket = "demo.challenge" #input("Nombre del bucket: ")
+access_key_id = input("Acces key id: ")
+secret_access_key = input("Secret Key: ")
+nbucket =  input("Nombre del bucket: ")
 
+# Logueo con las keys ingresadas
 s3 = boto3.resource(
         's3', 
         aws_access_key_id = access_key_id,
@@ -17,18 +18,18 @@ s3 = boto3.resource(
         config=Config(signature_version='s3v4')
     )
 
-
+# Se le solicita al usuario el código de la acción que desea realizar
 acc = input("1. Subir un archivo. 2. Descargar un archivo. Otra tecla. salir -> ")
 
 if int(acc) == 1 : #Upload
-    file = "test.png" #input(str("ingrese archivo que desea subir: "))
+    file = input(str("ingrese archivo que desea subir: ")) # Pedimos el nombre del archivo a subir
     data = open(file, 'rb')
     s3.Bucket(nbucket).put_object(Key = file,Body = data)
-    print("Subido.")
+    print("Subido.") # Informamos al usuario que la acción se realizó
 elif int(acc) == 2 : #Download
-    file = input(str("ingrese archivo que desea descargar: "))
+    file = input(str("ingrese archivo que desea descargar: ")) # Pedimos el nombre del archivo a bajar
     s3.Bucket(nbucket).download_file(file,"manu")
-    print("Descargado.")
+    print("Descargado.") # Informamos al usuario que la acción se realizó
 else :
     print("Sale del programa.")
     exit()
